da0a2125f029d49ec877b0a60a479350d0152913
Caden Lund <Cadenjlund@gmail.com>
2026-04-11 03:42:11 -0500

    added tests for GetUserByEmail and refactored user struct in user_test to global

--- apps/api/internal/repository/user_test.go
+++ apps/api/internal/repository/user_test.go
@@ -15,45 +15,46 @@ import (
 	"github.com/cadenlund/client-portal/internal/testutil"
 	"github.com/cadenlund/client-portal/internal/util"
 	"github.com/google/uuid"
+	"github.com/jackc/pgx/v5"
 	"github.com/jackc/pgx/v5/pgconn"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
 
-func TestCreate_User(t *testing.T) {
-	// Generic context
-	ctx := context.Background()
+// Use generated params struct for each method
+// cant use := in global scope
+var user = CreateUserParams{
+	Email:        "test@example.com",
+	PasswordHash: "hashedPassword",
+	Name:         util.Ptr("John Doe"), // Needed because we return address of for a pointer type
+	AvatarUrl:    nil,
+}
 
-	// Create our test models to use
-	user := CreateUserParams{
-		Email:        "test@example.com",
-		PasswordHash: "hashedPassword",
-		Name:         util.Ptr("John Doe"), // Needed because we return address of for a pointer type
-		AvatarUrl:    nil,
-	}
+func TestCreate_User(t *testing.T) {
+	ctx := context.Background() // generic, never canceled
 
 	t.Run("Create user: success", func(t *testing.T) {
 		//1. First create the queries dependency struct (Generated from sqlc)
 		// Use a transaction here that rolls back on every subtest
-		q := New(testutil.WithTx(t, testPool))
+		q := New(testutil.WithTx(t, testPool)) // pass t to register rollback on cleanup
 
 		//2. Insert user
-		got, err := q.CreateUser(ctx, user)
+		actual, err := q.CreateUser(ctx, user)
 
 		//3. Check error
 		require.NoError(t, err)
 
 		//4. Check inserted
-		assert.Equal(t, user.Email, got.Email)
-		assert.Equal(t, user.Name, got.Name)
-		assert.Equal(t, user.PasswordHash, got.PasswordHash)
-		assert.Nil(t, got.AvatarUrl)
+		assert.Equal(t, user.Email, actual.Email)
+		assert.Equal(t, user.Name, actual.Name)
+		assert.Equal(t, user.PasswordHash, actual.PasswordHash)
+		assert.Nil(t, actual.AvatarUrl)
 
 		//5. Check defaults
-		assert.NotEqual(t, uuid.Nil, got.ID)    // check if uuid is not the zero uuid
-		assert.Equal(t, UserRoleUser, got.Role) // Use UserRoleUser from generated models
-		assert.WithinDuration(t, time.Now(), got.CreatedAt, time.Second)
-		assert.WithinDuration(t, time.Now(), got.UpdatedAt, time.Second)
+		assert.NotEqual(t, uuid.Nil, actual.ID)    // check if uuid is not the zero uuid
+		assert.Equal(t, UserRoleUser, actual.Role) // Use UserRoleUser from generated models
+		assert.WithinDuration(t, time.Now(), actual.CreatedAt, time.Second)
+		assert.WithinDuration(t, time.Now(), actual.UpdatedAt, time.Second)
 	})
 
 	t.Run("Create user: duplicate email", func(t *testing.T) {
@@ -72,4 +73,33 @@ func TestCreate_User(t *testing.T) {
 		assert.Equal(t, "23505", pgErr.Code) // 23505  means unique contstraint violation
 	})
 
+}
+
+func TestGet_user(t *testing.T) {
+	ctx := context.Background()
+
+	t.Run("Test get user: success", func(t *testing.T) {
+		q := New(testutil.WithTx(t, testPool))
+
+		//1. Insert user
+		expected, err := q.CreateUser(ctx, user)
+		require.NoError(t, err)
+
+		//2. Get by email
+		actual, err := q.GetUserByEmail(ctx, user.Email)
+		require.NoError(t, err)
+
+		//3. assert
+		assert.Equal(t, expected, actual)
+	})
+
+	t.Run("Test get user: Not found", func(t *testing.T) {
+		q := New(testutil.WithTx(t, testPool))
+
+		//1. Get by email
+		_, err := q.GetUserByEmail(ctx, user.Email)
+
+		//2. Require error
+		require.ErrorIs(t, err, pgx.ErrNoRows)
+	})
 }
